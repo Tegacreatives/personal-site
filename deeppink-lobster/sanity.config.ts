d16fa8db41eb85e4d3747c104bed4ef35ef83d78
Tegacreatives <contact@tegacreatives.com>
2023-09-21 23:33:22 +0100

    moved config back to strapi folder

---
+++ deeppink-lobster/sanity.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'deeppink-lobster',
+
+  projectId: 'cv1zk67h',
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
