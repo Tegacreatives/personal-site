219aa7f4a9885daf56d459544b57868d56fce20d
Tegacreatives <contact@tegacreatives.com>
2023-09-22 15:37:39 +0100

    Added my image

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -8,9 +8,9 @@ const About = () => {
       <Title title="About Me" />
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
         <div
-          className="bg-cover min-h-[340px] md:min-h-[500px]"
+          className="bg-cover min-h-[420px] md:min-h-[500px]"
           style={{
-            backgroundImage: "url('./assets/headshot.jpg')",
+            backgroundImage: "url('./assets/tega.jpg')",
           }}
         ></div>
         <div className="space-y-10">
