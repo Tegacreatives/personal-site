1086bed1c274ae0d5995f9978dbafd7a08dcb9e1
Tegacreatives <contact@tegacreatives.com>
2023-09-25 12:19:46 +0100

    Updated about me

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -16,7 +16,7 @@ const About = () => {
         <div className="space-y-10">
           <div className="text-[26px] lg:text-[44px] font-normal lg:leading-[51.9px]">
             <span className="text-[#ACA0E8]">Hola!</span> My name is Tega
-            Okorare, a developer with a passion for building scalable and
+            Okorare, a developer with a focus on building scalable and
             accessible web experiences and products.
           </div>
           <div className="text-[18px] font-normal">
