f5e76aff2bdbdeaae5c7ba2ffcfd7f291bffc901
Tegacreatives <contact@tegacreatives.com>
2023-11-10 11:40:30 +0100

    Updated work experience

--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -4,9 +4,19 @@ import Accordion from "./Accordion";
 
 const workPlaces = [
   {
+    company: "Rooks",
+    role: "Full-Stack Developer",
+    timeFrame: "2022 - Current",
+    work: [
+      "Developed responsive e-commerce platform with over 200 users",
+      "Integrated secure payment gateway for seamless and secure payments",
+      "Implemented robust error handling for third-party APIs",
+    ],
+  },
+  {
     company: "Nigeria LNG",
     role: "Digital Innovation Intern",
-    timeFrame: "2022 - 2023",
+    timeFrame: "2022",
     work: [
       "Developed user interface for a new internal project management tool.",
       "Participated in code reviews and provided feedback to other developers.",
@@ -16,7 +26,7 @@ const workPlaces = [
   {
     company: "I.S.T.H",
     role: "Technical Support",
-    timeFrame: "2021",
+    timeFrame: "2020 - 2022",
     work: [
       "Offered network troubleshooting assistance to staff members, ensuring minimal disruptions in daily operations.",
       "Performed website updates and maintenance, contributing to an up-to-date and user-friendly online presence for the organization.",
