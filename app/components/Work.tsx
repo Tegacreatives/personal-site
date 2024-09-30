e78f4924aeb1bcca6730aa5261801168daef3902
Tegacreatives <tegaokorare91@gmail.com>
2024-09-30 22:52:21 +0100

    remove log message

--- app/components/Work.tsx
+++ app/components/Work.tsx
@@ -34,7 +34,7 @@ async function getProject() {
 
 const Work = async () => {
   const projects = (await getProject()) as ProjectProps[];
-  console.log(projects);
+
   return (
     <div className="flex flex-col w-full py-12">
       {/* section title */}
